abaef838e4ac389b365b60b411255a25e931b256
davidonuche <iamdavidonuche@gmail.com>
2026-09-15 23:03:23 +0100

    Initial Commit

---
+++ Frutus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Frutus
+//
+//  Created by David Onuche on 15/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
