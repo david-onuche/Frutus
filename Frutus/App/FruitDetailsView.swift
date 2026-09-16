a8724b4af0552b418a346e3c6546c383258fffeb
davidonuche <iamdavidonuche@gmail.com>
2026-09-16 16:45:47 +0100

    Done with Frutus Application

--- Frutus/App/FruitDetailsView.swift
+++ Frutus/App/FruitDetailsView.swift
@@ -56,6 +56,7 @@ struct FruitDetailsView: View {
             } //: SCROLL
             .edgesIgnoringSafeArea(.top)
         } //: NAVIGATION
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
--- Frutus/View/StartButtonView.swift
+++ Frutus/View/StartButtonView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct StartButtonView: View {
     // MARK: - PROPERTIES
-    @AppStorage("isOnbaording") var isOnboarding: Bool?
+    @AppStorage("isOnboarding") var isOnboarding: Bool = true
     
     // MARK: - BODY
     
